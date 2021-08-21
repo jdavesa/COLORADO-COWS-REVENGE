b30c9d046e36eaa7ef73b43bc721d8b03b6ca495
jdavesa <86001040+jdavesa@users.noreply.github.com>
2021-08-21 20:28:45 +0200

    Create index.js

---
+++ js/index.js
@@ -0,0 +1,39 @@
+/* 
+Juego de plataforma
+
+Cencepto: Actualización juego del dinosaurio del chrome cuando sucede un error 404, basada en un meme de internet
+
+MVP:
+
+- Desplazamiento lateral
+
+- Personaje fijo centro panel de canvas. Opciones de salto (espacio), agachr-se(flecha abajo) y sprint(comer elemento).
+
+- Elemento (logo wifi) misma velocidad que personaje
+
+- Collision con elementos landscape(no sprint) generan retraso.
+
+- Colision con elemento wifi genera game over
+
+///////////////////////////////////////////////////////////////////
+
+
+COLORADO COWS REVEANGE
+
+*/
+
+
+
+
+
+
+
+window.onload = () => {
+    
+    myApp.init(document.querySelector('#canvas'))
+   
+    
+}
+
+
+
